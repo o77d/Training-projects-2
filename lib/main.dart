425468cf92048f79a9c1fb63046ce696554f212a
MOHAMMED <ffoodd54321@gmail.com>
2026-03-15 18:35:20 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/splash.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          
+          elevation: 0,
+          
+        ),
+      ),
+
+      home: Splash(),
+    );
+  }
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/home.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 1), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (c) => (Home())));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column( 
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[SvgPicture.asset('assets/logo/Runway.svg')]),
+    );
+  }
+}
